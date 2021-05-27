01558ef64c1391e2223d0e7ca0818e1a9d211f7a
Matt James <matt@gettoken.com>
2021-05-27 22:10:31 +0800

    Remove DNS Malarky

--- main.tf
+++ main.tf
@@ -161,16 +161,3 @@ resource "aws_cloudwatch_metric_alarm" "cache_memory" {
   ok_actions    = var.ok_actions
   depends_on    = [aws_elasticache_replication_group.default]
 }
-
-module "dns" {
-  source  = "cloudposse/route53-cluster-hostname/aws"
-  version = "0.12.0"
-
-  enabled  = module.this.enabled && var.zone_id != "" ? true : false
-  dns_name = var.dns_subdomain != "" ? var.dns_subdomain : module.this.id
-  ttl      = 60
-  zone_id  = var.zone_id
-  records  = var.cluster_mode_enabled ? [join("", aws_elasticache_replication_group.default.*.configuration_endpoint_address)] : [join("", aws_elasticache_replication_group.default.*.primary_endpoint_address)]
-
-  context = module.this.context
-}
